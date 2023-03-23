7e41aa2ba7300026b49764b8ea0d57c9499550eb
Vittoria Ossanna <61164007+TudiFrudii@users.noreply.github.com>
2023-03-23 17:11:31 +0100

    minor fixes

--- PCA/scripts/exploratoryAnalysis_PCA.R
+++ PCA/scripts/exploratoryAnalysis_PCA.R
@@ -2,6 +2,8 @@
 library("GEOquery")
 library("useful")
 library("rgl")
+library("plotly")
+
 
 # import data from geo, extract data matrix
 gse <- getGEO("GSE84046", destdir = ".", getGPL = FALSE)
@@ -15,8 +17,8 @@ boxplot(ex, main = "Boxplot of raw values")
 
 # std way of dealing with missing values
 # remove all lines that han one or more omits
-ex2 <- na.omit(as.matrix(ex))
-dim(ex2)
+# ex2 <- na.omit(as.matrix(ex))
+# dim(ex2)
 # no need, no null values
 
 
@@ -30,17 +32,25 @@ gse$characteristics_ch1.2
 
 #set labels for pca
 labels = c()
+group_labels = c()
+complete_labels = c()
 for (x in 1:44) {
   # BEFORE SAMPLING -> control group
   if (gse$characteristics_ch1.2[x] == "time of sampling (before/after): before"){
     labels[x] <- "green"
+    group_labels[x] <- "control"
+    complete_labels[x] <- paste(colnames(ex)[x], "(", "control", ")")
   } 
   # AFTER SAMPLING
   else {
-    if (gse$characteristics_ch1.1[x] == "protein content restricted diet: high"){
-      labels[x] <-  "red"
+    if (gse$characteristics_ch1.1[x] == "protein content restricted diet: high") {
+      labels[x] <- "red"
+      group_labels[x] <- "ER_HP"
+      complete_labels[x] <- paste(colnames(ex)[x], "(", "ER_HP", ")")
     } else {
       labels[x] <- "blue"
+      group_labels[x] <- "ER_SP"
+      complete_labels[x] <- paste(colnames(ex)[x], "(", "ER_SP", ")")
     }
   }
 }
@@ -56,5 +66,6 @@ text(pca$x[,1], pca$x[,2], rownames(pca$x), cex=0.75)
 scores = as.data.frame(pca$x)
 plot3d(scores[,1:3], size=5, col = labels)
 
-
+t <- as.data.frame(pca$x)
+plot_ly(t, x~PC1, y~PC2, z~PC3, col = labels)
 
